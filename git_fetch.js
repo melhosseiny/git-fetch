bf7ef2e284bfa89aa8733fda89849a0f3bf9409f
Mostafa Elshamy <melhosseiny@users.noreply.github.com>
2021-07-24 11:58:27 +0200

    amend script for deno deploy

--- git_fetch.js
+++ git_fetch.js
@@ -1,25 +1,14 @@
-const server = Deno.listen({ port: 5000 });
-console.log(`HTTP webserver running. Access it at:  http://localhost:5000/`);
+addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
+  const rawContent = await (await fetch(`https://raw.githubusercontent.com/${url.pathname}`)).text();
 
-async function handle(conn) {
-  const httpConn = Deno.serveHttp(conn);
-  for await (const requestEvent of httpConn) {
-    const url = new URL(requestEvent.request.url);
-    console.log(url.pathname);
-    const rawContent = await (await fetch(`https://raw.githubusercontent.com/${url.pathname}`)).text();
-    console.log(rawContent);
-    await requestEvent.respondWith(
-      new Response(rawContent, {
-        status: 200,
-        headers: new Headers({
-          'Content-Type': url.pathname.endsWith(".css") ? "text/css" : "application/javascript",
-          'Access-Control-Allow-Origin': "*"
-        })
+  event.respondWith(
+    new Response(rawContent, {
+      status: 200,
+      headers: new Headers({
+        'Content-Type': url.pathname.endsWith(".css") ? "text/css" : "application/javascript",
+        'Access-Control-Allow-Origin': "*"
       })
-    );
-  }
-}
-
-for await (const conn of server) {
-  handle(conn);
-}
+    })
+  );
+});
